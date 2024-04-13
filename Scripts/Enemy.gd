5fdd09f9116cd6cac21c56afb75eccec74fe83b8
André Lourenço <alourenco.dev@gmail.com>
2024-04-13 19:33:39 +0200

    Prevent flitch when the enemy gets very close to the player

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -5,16 +5,33 @@ class_name Enemy;
 
 
 @export var speed = 200
+@export var health = 100
+@export var score = 1
+
+signal on_death(position : Vector2, socore : int)
 
 var target : Node2D
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	# An enemy needs to have a target
-	assert(target != null)
+	if (target == null):
+		pass
 
-	var direction = (target.position - position).normalized()
+	const target_distance_threshold = 80.0
+	print("Threshold ", target_distance_threshold)
+
+	var direction = (target.global_position - global_position)
+	var distance_to_target_sqr = direction.length_squared()
+	print("Distance: ", distance_to_target_sqr)
+	if (distance_to_target_sqr <= target_distance_threshold):
+		return
+
+	direction = direction.normalized()
 	position += direction * speed * delta
 
-	pass
+
+func apply_damage(amount):
+	health -= amount
+	if (amount < 0):
+		on_death.emit(global_position, score)
+		queue_free()
