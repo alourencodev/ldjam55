c9c65c57ef88421d5f73dc5161125539b5871950
André Lourenço <alourenco.dev@gmail.com>
2024-04-14 21:39:31 +0200

    Fix grenade player

--- Scripts/GrenadeHand.gd
+++ Scripts/GrenadeHand.gd
@@ -1,9 +1,19 @@
 extends Marker2D
 
 @export var grenade : PackedScene
+@export var damage = 10
+@export var fire_rate = 0.0
+
 @onready var _animated_sprite = $"../AnimatedSprite2D"
 
-func _process(_delta):
+
+var time_until_last_fire = fire_rate
+
+func _process(delta):
+	if (time_until_last_fire < fire_rate):
+		time_until_last_fire += delta
+		return
+
 	if Input.is_action_just_pressed("Action"):
 		throw()
 		_animated_sprite.play("attack")
@@ -11,5 +21,7 @@ func _process(_delta):
 func throw():
 	# TODO: Implement rate of fire
 	var grenade_instance = grenade.instantiate()
-	add_child(grenade_instance)
+	var manager = get_node("/root/Global/ProjectileManager")
 	grenade_instance.global_transform = global_transform
+	grenade_instance.damage = damage
+	manager.shoot(grenade_instance)
--- Scripts/grenade.gd
+++ Scripts/grenade.gd
@@ -1,24 +1,19 @@
 extends Area2D
 
-@onready var anim = $AnimationPlayer
 var speed = 350
+var damage = 10
 
-func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action("Action"):
-		attack()
+@onready var anim = $AnimationPlayer
 
-func attack():
+func _ready():
 	anim.play("grenade")
 
-
-func _on_body_entered(body: Node2D) -> void:
-		if body.has_method("handle_hit"):
-			body.handle_hit
+func _on_body_entered(body: Node2D):
+	if body.has_method("handle_hit"):
+		body.handle_hit(damage)
 
 func _process(delta):
 	position += transform.x * speed * delta
 
-func _on_bullet_body_entered(body):
-	if body.is_in_group("mobs"):
-		body.queue_free()
+func _on_ground_hit():
 	queue_free()
